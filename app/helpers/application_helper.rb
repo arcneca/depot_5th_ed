c8941d0dc7879f80fbd7c9804ba8b46a64b06b76
arkneca <stojanpro@gmail.com>
2016-11-24 18:37:47 +0100

    Complete task C playtime

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def currency_euro(item)
+    number_to_currency(item, :unit => "€",
+                             :separator => ",",
+                             :delimiter => "",
+                             :format => "%n %u")
+  end
 end
