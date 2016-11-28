3f9929b16f2fdee1f80378b524951b1ea9245189
psto <piotrstojan@gmail.com>
2016-11-28 22:27:45 +0100

    Build login for admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :set_date_loaded
+  before_action :authorize, :set_date_loaded
   protect_from_forgery with: :exception
+
+  protected
+
+    def authorize
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+      end
+    end
 
   private
 
